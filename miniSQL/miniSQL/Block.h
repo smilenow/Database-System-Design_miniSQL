ae20f1a4cd6ff9a869505c57d3d0ff01a2ca3eec
smilenow <troysmilenow@gmail.com>
2014-10-25 21:32:05 +0800

    update
    update block.h

--- miniSQL/miniSQL/Block.h
+++ miniSQL/miniSQL/Block.h
@@ -59,7 +59,7 @@ public:
 	bool get_dirty()const{return is_dirty;}
 	void set_dirty(const bool &dirty){is_dirty=dirty;}
 	virtual void calc_head_size()=0;
-	virtual ~Block()=0;
+    virtual ~Block(){};
 };
 
 #endif

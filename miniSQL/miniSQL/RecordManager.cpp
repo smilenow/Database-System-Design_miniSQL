eaef99b212004dd7456480d493f2673b5f89e144
smilenow <troysmilenow@gmail.com>
2014-10-25 21:34:11 +0800

    update
    update

---
+++ miniSQL/miniSQL/RecordManager.cpp
@@ -0,0 +1,9 @@
+//
+//  RecordManager.cpp
+//  miniSQL
+//
+//  Created by SmilENow on 10/25/14.
+//  Copyright (c) 2014 Jiaquan Yin. All rights reserved.
+//
+
+#include "RecordManager.h"
---
+++ miniSQL/miniSQL/RecordManager.h
@@ -0,0 +1,14 @@
+//
+//  RecordManager.h
+//  miniSQL
+//
+//  Created by SmilENow on 10/25/14.
+//  Copyright (c) 2014 Jiaquan Yin. All rights reserved.
+//
+
+#ifndef __miniSQL__RecordManager__
+#define __miniSQL__RecordManager__
+
+#include <stdio.h>
+
+#endif /* defined(__miniSQL__RecordManager__) */

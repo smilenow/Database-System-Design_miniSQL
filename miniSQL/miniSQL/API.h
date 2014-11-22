d6e190dd6d1215168d97d393ed70bc7d5dc482a7
sasameyuki <sasameyuki94@gmail.com>
2014-11-23 01:05:36 +0800

    I hope this will be the final version!!!!!!!

--- miniSQL/miniSQL/API.h
+++ miniSQL/miniSQL/API.h
@@ -23,22 +23,45 @@ extern IndexManager *indexmanager;
 
 class API{
 public:
+    // 对外的接口，解决所有的sql语句
 	Recordinfo dealCmd(sqlcommand& sql);
+
+    // 应catalogmanager要求，新增这些接口，供interpreter语法检查使用
 	int AttrCount(std::string tablename);
 	bool TableExists(std::string tablename);
 	bool IndexExists(std::string indexname);
 	bool AttrExists(std::string attrname,std::string tablename);
 public:
     // 全部返回recordinfo可能比较浪费？
+
+    // 先检查所有有索引的属性，然后对所有用索引查到的tuple取交
+    // 再判断这些tuple是否满足剩下的条件
     Recordinfo select(sqlcommand& sql);
+
+    // 先选择到要删除的tuple，同select
+    // 然后删除record，再删除该表所有的index里面对应的tuple
     Recordinfo del(sqlcommand& sql);
+    
+    // 先检查table是否存在，然后检查primary key/unique有没有重复
+    // 插入tuple，再插入所有的索引
     Recordinfo insert(sqlcommand& sql);
+
+    // 创建表的同时创建索引
     Recordinfo createTable(sqlcommand& sql);
+
+    // first check if it is a unique/primary
+    // then check a index has been created
+    // if created, return the index name
+    // 创建结束之后，还要：
+    // 在catalog里面更新记录
+    // 插入该表现在所有的record
     Recordinfo createIndex(sqlcommand& sql, bool flag=false);
+
+    // turn to Catalog find all index
+    // drop index, drop table in Catalog, drop all records
     Recordinfo dropTable(sqlcommand& sql);
+    
     Recordinfo dropIndex(sqlcommand& sql);
-    Recordinfo wrapTable();
-    Recordinfo wrapAttribute();
 };
 
 
--- miniSQL/miniSQL/BufferManager.h
+++ miniSQL/miniSQL/BufferManager.h
@@ -15,7 +15,7 @@
 #include <vector>
 #include <map>
 
-#define Buffer_Capacity 200
+#define Buffer_Capacity 5
 
 #define DB 1	// DataBlock
 #define IB 2	// IndexBlock
@@ -32,15 +32,17 @@ public:
 		write_all();
 	}
 
+	// 根据type识别不同的manager，new一个对应类型的block，初始化后返回
 	Block* newBlock(int type, std::string tablename, int NodeType=-100, int AttrType=-100, int valuecharlen=0);
 	// todo: catalog_block分开
 	// 读block可不分开，但是必须要指明是哪个manager
 	// 返回后需要dynamic_cast
+	// getBlock()根据类型，表名和bid获取一个block
 	Block* getBlock(int type, std::string tablename, int bid);
-	// storeBlock第一个参数可以去掉，只给出block就可以
+	// storeBlock()第一个参数可以去掉，只给出block就可以
 	bool storeBlock(std::string tablename, Block *block);
 	// 为了防止产生歧义，getblocknumber使用fname(file)做变量名
-	// 事实上只有index的fname相对特殊
+	// 获取某个数据文件或索引文件里面存有多少block
 	int get_block_number(int type, std::string fname);
     void delete_blocks(std::string tablename);
 
@@ -57,28 +59,30 @@ public:
     TableCatalogBlock* newTableCatalogBlocks();
     AttrCatalogBlock* newAttrCatalogBlocks();
     IndexCatalogBlock* newIndexCatalogBlocks();
-
+	// for index manager
 	std::vector<IndexBlock> load_tree(std::string indexname);
 	bool store_tree(std::string indexname, std::vector<IndexBlock> tree);
 	bool delete_tree(std::string indexname);
 
 public:
+	// 写回block的底层实现，无条件写回block_n指定的block
 	bool write_recordblock(int block_n);
 	bool write_catalogblock(int block_n);
 	bool write_indexblock(int block_n);
-
+	// 使用clock选择写回一个block
 	int write_block();
+	// 写回所有的block
 	void write_all();
-	// void create_data(std::string tablename);
-	// void create_catalog(std::string tablename);
-	// void create_index(std::string tablename, std::string indexname);
 	bool is_full();
+	// clock LRU
 	static void* clock_LRU(void *arg);
 	void LRU();
+	// 锁住与解锁
 	void pin_block(int block_n);
 	void unpin_block(int block_n);
 	int find_type(Block* block);
 	void load_block(int block_n, int type, std::string tablename, int bid);
+	// 获取一个可用的block，可能要写回
 	int get_available_block();
 public:
 	// 当前valid的block个数
@@ -90,7 +94,6 @@ public:
     static bool reference_bit[Buffer_Capacity];
 	static int reference_bit_count;
 	// filename存放对应的block的filename
-	// map并不适合这个结构，但一开始写的是map，后来修改了结构，没有把map改掉了
 	std::map<int, std::string> filename;
 	// 存放各种block
 	Block* buffer[Buffer_Capacity];
